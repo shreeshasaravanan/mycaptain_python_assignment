52cc5177c9b56f26448e049379f94b2771e998f5
shreeshasaravanan <124897149+shreeshasaravanan@users.noreply.github.com>
2023-02-09 20:26:20 +0530

    Add files via upload

---
+++ python2extensionpy.py
@@ -0,0 +1,9 @@
+f,e=input("enter the filename:").split('.')
+if e=="py":
+    print("python")
+elif e=="txt":
+    print("text file")
+elif e=="ppt":
+    print("power point")
+elif e=="xlsx":
+    print("excel")
