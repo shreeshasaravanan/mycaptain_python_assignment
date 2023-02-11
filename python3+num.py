8fb0d8842d67485ca8f1cc0f828473e16327f56c
shreeshasaravanan <124897149+shreeshasaravanan@users.noreply.github.com>
2023-02-11 13:24:08 +0530

    Add files via upload

---
+++ python3+num.py
@@ -0,0 +1,12 @@
+list1=[12,-7,5,64,-14]
+l1=[]
+for i in list1:
+    if i>0:
+         l1.append(i)
+print(l1)
+list2=[12,14,-95,3]
+l2=[]
+for j in list2:
+    if j>0:
+        l2.append(j)
+print(l2)
